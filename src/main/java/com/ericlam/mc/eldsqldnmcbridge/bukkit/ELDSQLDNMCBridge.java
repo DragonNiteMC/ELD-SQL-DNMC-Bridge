d33c71a7ac8e9b404aadd81278ece0e9fbc7575b
eric2788 <tsukiko852@gmail.com>
2022-05-23 18:42:30 +0800

    fix package name not exist

--- src/main/java/com/ericlam/mc/eldsqldnmcbridge/bukkit/ELDSQLDNMCBridge.java
+++ src/main/java/com/ericlam/mc/eldsqldnmcbridge/bukkit/ELDSQLDNMCBridge.java
@@ -1,8 +1,8 @@
 package com.ericlam.mc.eldsqldnmcbridge.bukkit;
 
 import chu77.eldependenci.sql.SQLInstallation;
-import com.dragonnite.mc.dnmc.core.main.DragonNiteMC;
-import com.dragonnite.mc.dnmc.core.managers.SQLDataSource;
+import com.dragonite.mc.dnmc.core.main.DragoniteMC;
+import com.dragonite.mc.dnmc.core.managers.SQLDataSource;
 import com.ericlam.mc.eld.BukkitManagerProvider;
 import com.ericlam.mc.eld.ELDBukkit;
 import com.ericlam.mc.eld.ELDBukkitPlugin;
@@ -19,7 +19,7 @@ public class ELDSQLDNMCBridge extends ELDBukkitPlugin {
     @Override
     public void bindServices(ServiceCollection collection) {
         SQLInstallation sqlInstallation = collection.getInstallation(SQLInstallation.class);
-        SQLDataSource sqlDataSource = DragonNiteMC.getAPI().getSQLDataSource();
+        SQLDataSource sqlDataSource = DragoniteMC.getAPI().getSQLDataSource();
         sqlInstallation.injectDataSource(sqlDataSource.getDataSource(), MySQL8Dialect.class);
     }
 
